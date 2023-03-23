4052c1efd916857e98914d837ab666565a331fa5
ts941229 <tsk941229@gmail.com>
2023-03-23 17:48:38 +0900

    first commit

---
+++ src/test/java/com/review/review1/Review1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.review1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Review1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
