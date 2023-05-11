3e4dbf2a14966b23267ce82b2ad39b7fd04b1310
ts941229 <tsk941229@gmail.com>
2023-05-11 19:47:23 +0900

    clear

--- src/main/java/com/review/review1/member/controller/MemberController.java
+++ src/main/java/com/review/review1/member/controller/MemberController.java
@@ -1,13 +1,11 @@
 package com.review.review1.member.controller;
 
 
-import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
